a0c9a1bc008c8c439e061ddc1f1b112723146e04
Zhang Bowen <chambowen@126.com>
2020-08-07 00:02:25 +0800

    fix the warning in c++

--- t_reweightClosure.py
+++ t_reweightClosure.py
@@ -33,13 +33,13 @@ reweight1D(clos_met, "MET [MeV]", f"plots/dRlh/clos_met_fr_os.pdf", "_met")
 clos = AnaTTbarTrueFake(tauid=False, isOS=True, rewrite="n_btag == 2 && n_jets >= 2 && mBB < 150000. && mTW > 60000.")
 
 print(f"{TermColor.OKBLUE}Applying to njets inclusive samples ... {TermColor.ENDC}")
-clos.applyWeightStep1(("HT", os.path.join(os.getcwd(), "include", "Reweight1D_njets.h")), True)
+clos.applyWeightStep1(("HT", os.path.join(os.getcwd(), "include", "Reweight1D_njets.h")), False)
 
 print(f"{TermColor.OKBLUE}Applying second step reweighting ... {TermColor.ENDC}")
-clos.applyWeightStep2(("dRbb", os.path.join(os.getcwd(), "include", f"Reweight1D_dRbb.h")), True)
+clos.applyWeightStep2(("dRbb", os.path.join(os.getcwd(), "include", f"Reweight1D_dRbb.h")), False)
 
 print(f"{TermColor.OKBLUE}Applying third step reweighting ... {TermColor.ENDC}")
-clos.applyWeightStep3(("dRTauLep", os.path.join(os.getcwd(), "include", f"Reweight1D_dRlh.h")), True)
+clos.applyWeightStep3(("dRTauLep", os.path.join(os.getcwd(), "include", f"Reweight1D_dRlh.h")), False)
 
 # parametrisations
 clos_mhh = TTbarTrueFakePlot(clos, "mHH", "weight_extra", (2000, 200000, 2200000), array.array(
