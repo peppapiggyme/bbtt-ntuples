8a400cc1cf6ec5bd7b8dc7c7ff8a10da4a777a72
Your Name <you@example.com>
2020-08-12 08:56:31 +0000

    naming

--- t_reweightClosure.py
+++ t_reweightClosure.py
@@ -23,8 +23,8 @@ clos.applyWeightStep3(("dRTauLep", os.path.join(os.getcwd(), "include", f"Reweig
 clos_tau_pt = TTbarTrueFakePlot(clos, "tau_pt", "weight_final", (980, 20000, 1000000), array.array(
     'd', [20000, 25000, 30000, 35000, 40000, 45000, 50000, 60000, 70000, 80000, 90000, 100000, 1000000]))
 clos_mhh = TTbarTrueFakePlot(clos, "mHH", "weight_extra", (1800, 200000, 2000000), array.array(
-    'd', [200000, 300000, 350000, 400000, 450000, 500000, 550000, 600000, 700000, 800000, 900000, 1000000, 1200000, 2000000]))
+    'd', [200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000, 1200000, 2000000]))
 
 # basically reweight again, but take the difference against 1 as uncertainty ...
 reweight1D(clos_tau_pt, "#tau_{had} p_{T} [MeV]", f"plots/dRlh/clos_tau_pt_fr_os.pdf", "_clos_tau_pt", drawOpt="HIST")
-reweight1D(clos_mhh, "#M_{HH} [MeV]", f"plots/dRlh/clos_mhh_fr_os.pdf", "_mhh", drawOpt="HIST")
+reweight1D(clos_mhh, "#M_{HH} [MeV]", f"plots/dRlh/clos_mhh_fr_os.pdf", "_clos_mhh", drawOpt="HIST")
