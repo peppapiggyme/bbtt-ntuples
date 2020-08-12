50c86921091fd5dc9a140588db92c69b40ea62e3
Zhang Bowen <chambowen@126.com>
2020-08-12 09:25:29 +0800

    new ntuples

--- t_reweightSyst_id.py
+++ t_reweightSyst_id.py
@@ -7,8 +7,8 @@ from analysis.plot import *
 
 R.gInterpreter.ProcessLine("ROOT::EnableImplicitMT();")
 
-#rwt = AnaTTbarTrueFake(tauid=True, isOS=True, path=f"{os.getcwd()}/../fr-ntuple-v6/")
-rwt = AnaTTbarTrueFake(tauid=True, isOS=True, rewrite="n_btag == 2 && n_jets >= 2 && mBB > 150000. && mTW > 150000.", path=f"{os.getcwd()}/../fr-ntuple-v6/")
+#rwt = AnaTTbarTrueFake(tauid=True, isOS=True, path=f"{os.getcwd()}/../fr-ntuple-v7/")
+rwt = AnaTTbarTrueFake(tauid=True, isOS=True, rewrite="n_btag == 2 && n_jets >= 2 && mBB > 150000. && mTW > 150000.", path=f"{os.getcwd()}/../fr-ntuple-v7/")
 
 #regionTeX = "lephad, OS, Mbb>150, MTW>40, Pass #tau ID"
 regionTeX = "lephad, OS, Mbb>150, MTW>150, Pass #tau ID"
--- t_reweightSyst_noid.py
+++ t_reweightSyst_noid.py
@@ -7,8 +7,8 @@ from analysis.plot import *
 
 R.gInterpreter.ProcessLine("ROOT::EnableImplicitMT();")
 
-rwt = AnaTTbarTrueFake(tauid=False, isOS=True, path=f"{os.getcwd()}/../fr-ntuple-v6/")
-#rwt = AnaTTbarTrueFake(tauid=False, isOS=True, rewrite="n_btag == 2 && n_jets >= 2 && mBB < 150000. && mTW > 60000.", path=f"{os.getcwd()}/../fr-ntuple-v6/")
+rwt = AnaTTbarTrueFake(tauid=False, isOS=True, path=f"{os.getcwd()}/../fr-ntuple-v7/")
+#rwt = AnaTTbarTrueFake(tauid=False, isOS=True, rewrite="n_btag == 2 && n_jets >= 2 && mBB < 150000. && mTW > 60000.", path=f"{os.getcwd()}/../fr-ntuple-v7/")
 
 regionTeX = "lephad, OS, Mbb>150, MTW>40, No #tau ID"
 #regionTeX = "lephad, OS, Mbb<150, MTW>60, No #tau ID"
